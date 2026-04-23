e597f3aa53e23f88cc769290d5eab4c697d2abd5
Marilene Costa <marileneromao@gmail.com>
2026-04-23 17:04:07 -0300

    Ajustando scripts para evitar erro de tabela existente

--- filmes/filmes/src/main/resources/schema.sql
+++ filmes/filmes/src/main/resources/schema.sql
@@ -1,15 +1,14 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE genero (
+CREATE TABLE IF NOT EXISTS genero (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE filme (
-    -- Mudamos de SERIAL para UUID
+CREATE TABLE IF NOT EXISTS filme (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(), 
     titulo VARCHAR(200) NOT NULL,
-    ano INTEGER,                     
+    ano INTEGER,                    
     diretor VARCHAR(150),           
     genero_id INTEGER,               
     FOREIGN KEY (genero_id) REFERENCES genero(id)
