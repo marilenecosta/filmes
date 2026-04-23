e5d04fd5c2ea2600fc0189378d9666ce7163a5cc
Marilene Costa <marileneromao@gmail.com>
2026-04-23 16:57:38 -0300

    Adicionando generos iniciais

---
+++ filmes/filmes/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO genero (nome) VALUES ('ACAO');
+INSERT INTO genero (nome) VALUES ('COMEDIA');
+INSERT INTO genero (nome) VALUES ('DRAMA');
+INSERT INTO genero (nome) VALUES ('FICCAO CIENTIFICA');
+INSERT INTO genero (nome) VALUES ('INFANTIL');
+INSERT INTO genero (nome) VALUES ('ROMANCE');
+INSERT INTO genero (nome) VALUES ('TERROR');
